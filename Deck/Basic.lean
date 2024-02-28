53a359ac98f0c8be11d2fa2d712a81c1a9015623
Tony Beta Lambda <tonybetalambda@gmail.com>
2024-02-28 19:34:47 +0800

    feat: implement serialization for layouts

--- Deck/Basic.lean
+++ Deck/Basic.lean
@@ -4,8 +4,11 @@ abbrev Suit := Fin 4
 
 notation "♠" => 0
 notation "♥" => 1
-notation "♢" => 2
+notation "♦" => 2
 notation "♣" => 3
+
+instance : ToString Suit where
+  toString s := .mk [#['♠', '♥', '♦', '♣'].get s]
 
 inductive Color where
   | red : Color
@@ -16,7 +19,7 @@ def Color.of (suit : Suit) : Color :=
   match suit with
   | ♠ => .black
   | ♥ => .red
-  | ♢ => .red
+  | ♦ => .red
   | ♣ => .black
 
 abbrev Rank := Fin 13
@@ -25,6 +28,12 @@ structure Card where
   suit : Suit
   rank : Rank
 deriving DecidableEq
+
+instance : ToString Rank where
+  toString r := .mk [#['A', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K'].get r]
+
+instance : ToString Card where
+  toString c := toString c.suit ++ toString c.rank
 
 @[reducible]
 def Card.color (card : Card) : Color := Color.of card.suit
--- FreeCell/Basic.lean
+++ FreeCell/Basic.lean
@@ -1,3 +1,4 @@
+import Lean.Data.Json
 import SizedArray.Basic
 import Deck.Basic
 
@@ -6,10 +7,21 @@ Defines FreeCell layouts
 -/
 
 open Deck
+open Lean
 
 abbrev Column := Array Card
 
 structure Layout (c n : Nat) where
   columns : SizedArray Column c
-  foundations : SizedArray (Option Rank) 4
   cells : SizedArray (Option Card) n
+  foundations : SizedArray (Option Rank) 4
+
+instance : ToJson Card where
+  toJson card := toString card
+
+instance (c n : Nat) : ToJson (Layout c n) where
+  toJson layout := Json.mkObj [
+    ("columns", toJson <| layout.columns.1),
+    ("cells", toJson <| layout.cells.1),
+    ("foundations", toJson <| layout.foundations.1.map λ o => (Option.casesOn o "" toString : String))
+  ]
