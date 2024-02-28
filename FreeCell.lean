10ee6cf3359a3b15971303b2b30b282acd92e4da
Tony Beta Lambda <tonybetalambda@gmail.com>
2024-02-28 19:04:27 +0800

    feat: add user-facing helper functions

--- FreeCell.lean
+++ FreeCell.lean
@@ -2,3 +2,4 @@
 -- Import modules here that should be built as part of the library.
 import FreeCell.Basic
 import FreeCell.CanBuildOn
+import FreeCell.Moves
--- FreeCell/Moves.lean
+++ FreeCell/Moves.lean
@@ -11,16 +11,21 @@ namespace Layout
 variable {k n : Nat}
 variable (τ : Layout k n)
 
-/-- A card can be moved from a nonempty column or a cell -/
+/-- A card can reside at a column, a cell, or the foundation -/
+inductive Pos (k n : Nat) where
+  | column : Fin k → Pos k n
+  | cell : Fin n → Pos k n
+  | foundation : Pos k n
+
+/-- but can be only moved from a nonempty column or cell -/
 inductive From where
   | column (i : Fin k) : τ.columns[i].size > 0 → From
   | cell (i : Fin n) : τ.cells[i] ≠ .none → From
 
-/-- to a column, a cell, or a foundation -/
-inductive To (k n : Nat) where
-  | column : Fin k → To k n
-  | cell : Fin n → To k n
-  | foundation : To k n
+def validate : Pos k n → Option (From τ)
+  | .column i => if h : τ.columns[i].size > 0 then .some <| .column i h else .none
+  | .cell i => if h : τ.cells[i] ≠ .none then .some <| .cell i h else .none
+  | .foundation => .none
 
 def take (f : From τ) : Card × Layout k n :=
   match f with
@@ -35,7 +40,7 @@ def take (f : From τ) : Card × Layout k n :=
       | .none => by contradiction
     ⟨ card, { τ with cells := τ.cells.set i .none } ⟩
 
-def tryPut (c : Card) (t : To k n) : Option (Layout k n) :=
+def tryPut (c : Card) (t : Pos k n) : Option (Layout k n) :=
   match t with
   | .column j =>
     let column := τ.columns[j]
@@ -61,5 +66,9 @@ def tryPut (c : Card) (t : To k n) : Option (Layout k n) :=
         .some { τ with foundations := τ.foundations.set j (.some c.rank) }
       else
         .none
+
+def move (f t : Pos k n) : Option (Layout k n) := do
+  let (card, τ') := τ.take <| ← τ.validate f
+  τ'.tryPut card t
 
 end Layout
